8f4e2b7114c78f069047065a235e490e64f80872
Kristen <kristenamorgan@mac.com>
2013-03-23 20:35:07 -0500

    Begun implementation of Google Maps API

--- COMP3900/NCMS/src/com/ncms/ncms/MainActivity_tab.java
+++ COMP3900/NCMS/src/com/ncms/ncms/MainActivity_tab.java
@@ -1,16 +1,36 @@
 package com.ncms.ncms;
 
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.SimpleExpandableListAdapter;
 
 public class MainActivity_tab extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		try{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_activity_tab);
+		
+		List<? extends Map<String, ?>> manifest_list = null;
+		int groupLayout =0, childLayout = 0;
+		int[] groupTo = new int[10];
+		int[] childTo = new int[10];
+		String[] groupFrom = new String[10];
+		String[] childFrom = new String[10];
+		List<? extends List<? extends Map<String, ?>>> childData = null;
+		SimpleExpandableListAdapter expListAdapter =
+            new SimpleExpandableListAdapter(this, manifest_list, groupLayout, groupFrom, groupTo, childData , childLayout, childFrom, childTo);
+            setListAdapter( expListAdapter );       // setting the adapter in the list.
+ 
+        }catch(Exception e){
+            System.out.println("Errrr +++ " + e.getMessage());
+        }
 		
 //		findViewById(R.id.sign_in_button).setOnClickListener(
 //				new View.OnClickListener() {
@@ -19,6 +39,11 @@ public class MainActivity_tab extends Activity {
 //						attemptLogin();
 //					}
 //				});
+	}
+
+	private void setListAdapter(SimpleExpandableListAdapter expListAdapter) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
