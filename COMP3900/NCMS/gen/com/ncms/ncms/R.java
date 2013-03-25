ddd69a0c5b46bb46df23141381ad81b0f43509f5
Kristen <kristenamorgan@mac.com>
2013-03-24 22:34:17 -0500

    More attempts at google maps api I have no idea what's going on

--- COMP3900/NCMS/gen/com/ncms/ncms/R.java
+++ COMP3900/NCMS/gen/com/ncms/ncms/R.java
@@ -17,28 +17,28 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f08000c;
-        public static final int action_forgot_password=0x7f08000e;
-        public static final int action_settings=0x7f08000d;
-        public static final int email=0x7f080004;
-        public static final int expandableListView1=0x7f080008;
-        public static final int login=0x7f080006;
-        public static final int login_form=0x7f080003;
-        public static final int login_status=0x7f080001;
-        public static final int login_status_message=0x7f080002;
-        public static final int map=0x7f080000;
-        public static final int password=0x7f080005;
-        public static final int sign_in_button=0x7f080007;
-        public static final int tab1=0x7f080009;
-        public static final int tab2=0x7f08000a;
-        public static final int tab3=0x7f08000b;
+        public static final int LinearLayout01=0x7f09000c;
+        public static final int action_forgot_password=0x7f09000e;
+        public static final int action_settings=0x7f09000d;
+        public static final int email=0x7f090004;
+        public static final int expandableListView1=0x7f090008;
+        public static final int login=0x7f090006;
+        public static final int login_form=0x7f090003;
+        public static final int login_status=0x7f090001;
+        public static final int login_status_message=0x7f090002;
+        public static final int mapview=0x7f090000;
+        public static final int password=0x7f090005;
+        public static final int sign_in_button=0x7f090007;
+        public static final int tab1=0x7f090009;
+        public static final int tab2=0x7f09000a;
+        public static final int tab3=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_google_map_tab=0x7f030000;
@@ -47,32 +47,31 @@ public final class R {
         public static final int activity_main_phone=0x7f030003;
     }
     public static final class menu {
-        public static final int agent_activity_tab=0x7f070000;
-        public static final int google_map=0x7f070001;
-        public static final int login=0x7f070002;
-        public static final int main=0x7f070003;
-        public static final int main_activity_tab=0x7f070004;
+        public static final int google_map=0x7f080000;
+        public static final int login=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int main_activity_tab=0x7f080003;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f05000b;
-        public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050009;
-        public static final int action_sign_in_short=0x7f05000a;
-        public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050010;
-        public static final int error_incorrect_password=0x7f05000f;
-        public static final int error_invalid_email=0x7f05000d;
-        public static final int error_invalid_password=0x7f05000e;
-        public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f05000c;
+        public static final int action_forgot_password=0x7f06000b;
+        public static final int action_settings=0x7f060001;
+        public static final int action_sign_in_register=0x7f060009;
+        public static final int action_sign_in_short=0x7f06000a;
+        public static final int app_name=0x7f060000;
+        public static final int error_field_required=0x7f060010;
+        public static final int error_incorrect_password=0x7f06000f;
+        public static final int error_invalid_email=0x7f06000d;
+        public static final int error_invalid_password=0x7f06000e;
+        public static final int hello_world=0x7f060002;
+        public static final int login_progress_signing_in=0x7f06000c;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050007;
-        public static final int prompt_password=0x7f050008;
-        public static final int title_activity_agent_activity_tab=0x7f050004;
-        public static final int title_activity_google_map=0x7f050005;
-        public static final int title_activity_login=0x7f050006;
-        public static final int title_activity_main_activity_tab=0x7f050003;
+        public static final int prompt_email=0x7f060007;
+        public static final int prompt_password=0x7f060008;
+        public static final int title_activity_agent_activity_tab=0x7f060004;
+        public static final int title_activity_google_map=0x7f060005;
+        public static final int title_activity_login=0x7f060006;
+        public static final int title_activity_main_activity_tab=0x7f060003;
     }
     public static final class style {
         /** 
@@ -98,11 +97,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int LoginFormContainer=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int LoginFormContainer=0x7f070002;
+    }
+    public static final class xml {
+        public static final int config=0x7f040000;
     }
 }
