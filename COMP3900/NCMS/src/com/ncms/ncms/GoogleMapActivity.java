426e210617c5ba86d13fd0ef2476d1d55de2ec99
Kristen <kristenamorgan@mac.com>
2013-03-28 00:36:14 -0500

    Stuff

--- COMP3900/NCMS/src/com/ncms/ncms/GoogleMapActivity.java
+++ COMP3900/NCMS/src/com/ncms/ncms/GoogleMapActivity.java
@@ -1,9 +1,13 @@
 package com.ncms.ncms;
 
-import java.util.List;
-
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 //import com.google.android.maps.MyLocationOverlay;
 //import com.google.android.maps.Overlay;
 
@@ -15,33 +19,44 @@ import android.os.Bundle;
 public class GoogleMapActivity extends Activity {
 
 	GoogleMap map;
-	long start,stop;
-	//MyLocationOverlay compass;
+	GoogleMapOptions option = new GoogleMapOptions();
+	Marker hamburg, kiel;
+	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	static final LatLng KIEL = new LatLng(53.551, 9.993);
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 		
+		if(map==null){
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 		        .getMap();
+		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+		option.zoomControlsEnabled(true);
 		
-//		Touch t = new Touch();
-//		List<Overlay> overlayList = map.getOverlays();
-//		overlayList.add(t);
-	}
+		}
+		
+		
+		if (map!=null){
+		      hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+		          .title("Hamburg"));
+		      kiel = map.addMarker(new MarkerOptions()
+		          .position(KIEL)
+		          .title("Kiel")
+		          .snippet("Kiel is cool")
+		          .icon(BitmapDescriptorFactory
+		              .fromResource(R.drawable.ic_launcher)));
+		    }
+		
+		map.setOnMarkerClickListener(new OnMarkerClickListener(){
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.map, menu);
-//		return true;
-//	}
-	
-//	class Touch extends Overlay{
-//		public boolean onTouchEvent(MotionEvent e, MapView m){
-//			return false;
-//		}
-//	}
-
-}
+			@Override
+			public boolean onMarkerClick(Marker arg0) {
+				// TODO Auto-generated method stub
+				//go to details page
+				return false;
+			}
+			
+		});
+	}}
